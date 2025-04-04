75b35327d49a32c21bd059046ca810312a43ae35
Harsh Shah <harsh.shah@siimteq.com>
2025-04-04 13:31:33 +0530

    wip

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 import clip
 import torch
+from PIL import Image
+import numpy as np
+import io
 
 app = FastAPI()
 
 # Load CLIP model
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
+
+
+def extract_image_vector(image):
+    image = preprocess(image).unsqueeze(0).to(device)  # Preprocess & add batch dim
+    with torch.no_grad():
+        features = model.encode_image(image)
+    return features.cpu().numpy().astype(np.float32).tolist()[0]
+
 
 @app.get('/')
 def index():
@@ -16,7 +27,15 @@ def index():
 
 
 @app.post('/img-to-vector')
-def to_vector():
-    return {
-        "message": "ok"
-    }
+async def to_vector(file: UploadFile = File(...)):
+    image = Image.open(io.BytesIO(await file.read())).convert("RGB")
+
+    try:
+        vector = extract_image_vector(image)
+        return {
+            "vector": vector
+        }
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
