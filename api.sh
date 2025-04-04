c6e8ea8ef3062da417793b40822edb8ca210b4db
Harsh Shah <harsh.shah@siimteq.com>
2025-04-04 10:13:42 +0530

    Wip

--- api.sh
+++ api.sh
@@ -1,4 +1,4 @@
-curl -X 'POST' 'http://localhost:8000/convert-image-to-vector' \
+curl -X 'POST' 'http://localhost:8090/convert-image-to-vector' \
 -H 'accept: application/json' \
 -H 'Content-Type: multipart/form-data' \
 -F 'file=@test_image.jpg'
