947163dce5b8596a0258f9f2f72cdf9178687677
Harsh Shah <harsh.siimteq@gmail.com>
2025-05-28 09:49:39 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -66,3 +66,7 @@ async def vector_from_image_url(payload: ImageURLRequest):
             "status": "error",
             "error": str(e)
         })
+    
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=5505, reload=True)
